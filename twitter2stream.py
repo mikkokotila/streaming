7898117af5b6c5b21944d4a8489c598b864a75ed
mikkokotila <mailme@mikkokotila.com>
2016-10-15 08:27:31 +0200

    Update twitter2stream.py

--- twitter2stream.py
+++ twitter2stream.py
@@ -16,18 +16,18 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
  
 class MyListener(StreamListener):
- 
+
+    def on_error(self, status):
+        print(status)
+        return True
+    
     def on_data(self, data):
         try:
-            with open('streaming.json', 'a') as f:
+            with open('/home/ubuntu/dep-panel.json', 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
             print("&quot;Error on_data: %s&quot; % str(e)")
-        return True
- 
-    def on_error(self, status):
-        print(status)
         return True
 
 twitter_stream = Stream(auth, MyListener())
